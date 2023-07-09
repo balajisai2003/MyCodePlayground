f4a372b513ddded2704524337e532f96970c0750
BalajiSai2003 <balajisai2003@outlook.com>
2023-07-09 18:33:53 +0530

    binary operations

---
+++ DSA/Bitmanupilation/binoperations.py
@@ -0,0 +1,7 @@
+print(5 and 4) # AND operation
+print(5 or 4) # OR operation
+print(5 ^ 6) #XOR operation
+print(~5) # ONE's complement operation
+print(5<<2) # LEFT SHIFT opertion a<<b = a*2^b
+print(5>>2) # RIGHT SHIFT opertion a>>b = a/2^b
+
--- DSA/rough.py
+++ DSA/rough.py
@@ -1,2 +1,4 @@
 for i in range(10,-1,-1):
     print(i)
+
+print(5 and 4)
