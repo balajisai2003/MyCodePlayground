c3f3729ed7ba599900a91c5821374e1b993026f5
BalajiSai2003 <balajisai2003@outlook.com>
2024-03-13 19:51:15 +0530

    BasicsOfFunctions

---
+++ javascript/BasicsOfFunctions.js
@@ -0,0 +1,17 @@
+// functions
+
+function greet(name) {
+  console.log('Hello ' + name);
+  return "Hello " + name
+}
+
+name = 'Balaji';
+
+greet(name); // Hello Balaji, name is parameter
+
+greet("Sai"); // Hello Sai, "Sai" is argument
+
+console.log(greet(name))
+
+
+
